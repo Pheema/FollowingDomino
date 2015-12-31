f00da3821df4b9b70c788a4b32f60212ff321cb9
Pheema <y.itou.physics@gmail.com>
2015-12-31 15:48:35 +0900

    メンバ変数の整理

--- Assets/Pitagora/Scripts/Camera/CameraRigController.cs
+++ Assets/Pitagora/Scripts/Camera/CameraRigController.cs
@@ -2,22 +2,41 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class CameraRigController : MonoBehaviour {
-    public float smoothTime = 0.7f;
-    // カメラとオブジェクト間の距離
-    public float distance = 10f;
+public class CameraRigController : MonoBehaviour
+{
+
+    #region Member variables
+
+    // ターゲットに近づくまでの時間
+    [SerializeField, Range(0f, 1f)]
+    float m_smoothTime = 0.7f;
+
     // 極座標
-    public float phi, theta;
-    public float rotationSpeed = 5f;
+    public float m_phi, m_theta;
+    public float m_rotationSpeed = 1f;
 
-    public Transform m_InitalTarget;
+    // 初期ターゲット
+    [SerializeField]
+    Transform m_InitalTarget;
 
-    Vector3 m_targetPos = Vector3.zero;
+    // ターゲットの座標
+    Vector3 m_targetPos;
+    public Vector3 targetPos
+    {
+        get { return m_targetPos; }
+    }
+    
+    // カメラの速度
     Vector3 m_cameraVelocity;
 
-    public float detectDuration = 0.1f;
+    // 衝突イベントを保持する時間
+    [SerializeField]
+    float m_detectDuration = 1.0f;
+
     // 衝突時刻、オブジェクトの履歴を保持するキュー
     Queue<KeyValuePair<float, Transform>> recentCollEvents = new Queue<KeyValuePair<float, Transform>>();
+
+    #endregion
 
     void Start()
     {
@@ -30,26 +49,22 @@ public class CameraRigController : MonoBehaviour {
         // それ以外は時間が経過したら消去する
         if (recentCollEvents.Count > 1)
         {
-            if (Time.time - recentCollEvents.Peek().Key > detectDuration)
+            if ((Time.time - recentCollEvents.Peek().Key) > m_detectDuration)
             {
                 recentCollEvents.Dequeue();
             }
         }
 
-        // カメラの回転
+        // キー操作によるカメラの回転
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-        phi += h * rotationSpeed;
-        theta += v * rotationSpeed;
-        theta = Mathf.Clamp(theta, -90f, 90f);
+        m_phi += h * m_rotationSpeed;
+        m_theta += v * m_rotationSpeed;
+        m_theta = Mathf.Clamp(m_theta, -90f, 90f);
     }
 
     void FixedUpdate()
     {
-        // カメラの回転操作
-        transform.rotation = Quaternion.Euler(new Vector3(theta, -phi, 0));
-        //transform.Rotate(new Vector3(theta, phi, 0), Space.Self);
-
         // 衝突イベントの平均位置を取得
         m_targetPos = Vector3.zero;
         foreach (var val in recentCollEvents)
@@ -57,8 +72,9 @@ public class CameraRigController : MonoBehaviour {
             m_targetPos += val.Value.position;
         }
         m_targetPos /= recentCollEvents.Count;
-        
-        transform.position = Vector3.SmoothDamp(transform.position, m_targetPos, ref m_cameraVelocity, smoothTime);
+
+        transform.position = Vector3.SmoothDamp(transform.position, m_targetPos, ref m_cameraVelocity, m_smoothTime);
+        transform.rotation = Quaternion.Euler(new Vector3(m_theta, -m_phi, 0));
     }
 
     public void AddCollEvent(Transform newTransform)
